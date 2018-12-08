a71fad2ebb8555bb41debc90860e371d7a02296a
TPettis <42744979+TPettis@users.noreply.github.com>
2018-12-08 17:45:17 -0500

    initial commit

---
+++ src/RentalDB/DBConnection.java
@@ -0,0 +1,10 @@
+package RentalDB;
+
+public interface DBConnection {
+
+  // database URL, username and password
+  String DATABASE_URL = "jdbc:derby:lib\\rentalcarDB";
+  String USERNAME = "user";
+  String PASSWORD = "password";
+
+}
